3b0d0865f4e537427d56ecc501004c6a67f2f1c4
Juan Jose Sarabia <jujosaca2013@hotmail.com>
2020-02-13 10:57:09 -0500

    Listar listo

--- Frontend/js/controlador.js
+++ Frontend/js/controlador.js
@@ -3,61 +3,90 @@ $(document).ready(function(){
 
   $.getJSON("/listar")
             .done(function (respuestaServ) {
-
-                
-
-                 arreglo = respuestaServ.usuarios;
+             
+                arreglo = respuestaServ.usuarios;
    					     
 
                 
 
-   					for (var i = 0; i < cantidad; i++) {
-
-                zode = arreglo[i].zode;
-                nombre = arreglo[i].nombre;
-                nombreI = arreglo[i].nombreI;
-                ngerente = arreglo[i].ngerente;
-                telefono = arreglo[i].telefono;
-                fechaFinal = arreglo[i].fechaFinal;
-                dias= arreglo[i].dias;
+   					for (var i = 0; i < respuestaServ.usuarios.length; i++) {
+                
+            
+             let   identificacion = arreglo[i].identificacion;
+             let   tipoId = arreglo[i].tipoId;
+             let   primerNombre = arreglo[i].primerNombre;
+             let   segundoNombre = arreglo[i].segundoNombre;
+             let   primerApellido = arreglo[i].primerApellido;
+             let   segundApellido = arreglo[i].segundApellido;
+             let   direccion= arreglo[i].direccion;
+             let   telefono= arreglo[i].telefono;
+             let   email= arreglo[i].email;
+             let   ocupacion= arreglo[i].ocupacion;
+             let   departamento = arreglo[i].departamento;
+             let   municipio= arreglo[i].municipio;
 
 
                                     var newTr= document.createElement("tr");
                                     var newTd1 = document.createElement("td");
                                     var newB1 = document.createElement("span");
                                     newB1.setAttribute("class","ui-table-cell-label");
-                                    newB1.innerHTML=zode;
+                                    newB1.innerHTML=identificacion;
 
                                     var newTd2 = document.createElement("td");
                                     var newB2 = document.createElement("span");
                                     newB2.setAttribute("class","ui-table-cell-label");
-                                    newB2.innerHTML=nombre;
+                                    newB2.innerHTML=tipoId;
 
                                     var newTd3 = document.createElement("td");
                                     var newB3 = document.createElement("span");
                                     newB3.setAttribute("class","ui-table-cell-label");
-                                    newB3.innerHTML=nombreI;
+                                    newB3.innerHTML=primerNombre;
 
                                     var newTd4 = document.createElement("td");
                                     var newB4 = document.createElement("span");
                                     newB4.setAttribute("class","ui-table-cell-label");
-                                    newB4.innerHTML=ngerente;
+                                    newB4.innerHTML=segundoNombre;
 
                                     var newTd5 = document.createElement("td");
                                     var newB5 = document.createElement("span");
                                     newB5.setAttribute("class","ui-table-cell-label");
-                                    newB5.innerHTML=telefono;
+                                    newB5.innerHTML=primerApellido;
 
                                     var newTd6 = document.createElement("td");
                                     var newB6 = document.createElement("span");
                                     newB6.setAttribute("class","ui-table-cell-label");
-                                    newB6.innerHTML=fechaFinal;
+                                    newB6.innerHTML=segundApellido;
 
                                     var newTd7 = document.createElement("td");
                                     var newB7 = document.createElement("span");
                                     newB7.setAttribute("class","ui-table-cell-label");
-                                    newB7.innerHTML=dias;
-                                    var a = parseInt(dias);
+                                    newB7.innerHTML=direccion;
+
+                                    var newTd8 = document.createElement("td");
+                                    var newB8 = document.createElement("span");
+                                    newB8.setAttribute("class","ui-table-cell-label");
+                                    newB8.innerHTML=telefono;
+
+                                    var newTd9 = document.createElement("td");
+                                    var newB9 = document.createElement("span");
+                                    newB9.setAttribute("class","ui-table-cell-label");
+                                    newB9.innerHTML=email;
+
+                                    var newTd10 = document.createElement("td");
+                                    var newB10 = document.createElement("span");
+                                    newB10.setAttribute("class","ui-table-cell-label");
+                                    newB10.innerHTML=ocupacion;
+
+                                    var newTd11 = document.createElement("td");
+                                    var newB11 = document.createElement("span");
+                                    newB11.setAttribute("class","ui-table-cell-label");
+                                    newB11.innerHTML=departamento;
+
+                                    var newTd12 = document.createElement("td");
+                                    var newB12 = document.createElement("span");
+                                    newB12.setAttribute("class","ui-table-cell-label");
+                                    newB12.innerHTML=municipio;
+                                   
 
                                     
 
@@ -86,7 +115,24 @@ $(document).ready(function(){
                                       newTr.appendChild(newTd7);
                                       newTd7.appendChild(newB7);
 
+                                      newTr.appendChild(newTd8);
+                                      newTd8.appendChild(newB8);
 
+                                      newTr.appendChild(newTd9);
+                                      newTd9.appendChild(newB9);
+
+                                      newTr.appendChild(newTd10);
+                                      newTd10.appendChild(newB10);
+
+                                      newTr.appendChild(newTd11);
+                                      newTd11.appendChild(newB11);
+
+                                      newTr.appendChild(newTd12);
+                                      newTd12.appendChild(newB12);
+
+                                      
+
+           
 
           }//fin del for
         } );
